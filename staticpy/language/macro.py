9b50d7d3dffe603f418368d24f7431610259183e
SnowWalkerJ <jike3212001@163.com>
2019-09-27 11:16:41 +0800

    @auto_add

--- staticpy/language/macro.py
+++ staticpy/language/macro.py
@@ -1,5 +1,5 @@
 from .session import get_session
-from .statement import Statement
+from .statement import Statement, auto_add
 from .block import Block
 
 
@@ -30,12 +30,14 @@ class IfDefMacro(Macro, Block):
         return stmts
 
 
+@auto_add
 def include(name):
     if not (name[0] == "<" and name[-1] == ">"):
         name = "\"" + name + "\""
     return StatementMacro("include", name)
 
 
+@auto_add
 def defineM(name, value=None):
     if value is not None:
         return StatementMacro("define", name, value)
@@ -43,25 +45,31 @@ def defineM(name, value=None):
         return StatementMacro("define", name)
 
 
+@auto_add
 def undefineM(name):
     return StatementMacro("undef", name)
 
 
+@auto_add
 def ifdefM(name):
     return StatementMacro("ifdef", name)
 
 
+@auto_add
 def ifndefM(name):
     return StatementMacro("ifndef", name)
 
 
+@auto_add
 def elseM(name):
     return StatementMacro("else")
 
 
+@auto_add
 def endifM(name):
     return StatementMacro("endif")
 
 
+@auto_add
 def ifdefBM(name):
     return IfDefMacro(name)
